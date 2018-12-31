4aabbfd60629d90949e3712906f75d3b49d50513
Tim Malone <tdmalone@gmail.com>
2018-12-31 17:37:57 +1100

    WIP: Continue custom data enrichment with distance calculation

--- enrich.py
+++ enrich.py
@@ -4,12 +4,22 @@ Custom data enrichments for the copied rows.
 @author Tim Malone <tim@timmalone.id.au>
 """
 
+from os import getenv
+from geopy import distance
+
+HOME_LATLNG = getenv('HOME_LATLNG')
+WORK_LATLNG = getenv('WORK_LATLNG')
+
 def enrich(item):
 
   new_attributes = {}
 
-  new_attributes['distance_from_home'] = 0
-  new_attributes['distance_from_work'] = 0
+  # Determine the current proximity to both work and home.
+  home_coords = HOME_LATLNG.split(',')
+  work_coords = WORK_LATLNG.split(',')
+  current_coords = (item['event_latitude']['S'], item['event_longitude']['S'])
+  new_attributes['distance_from_home'] = distance.geodesic(home_coords, current_coords).m
+  new_attributes['distance_from_work'] = distance.geodesic(work_coords, current_coords).m
 
   new_attributes['time_from_home_driving'] = 0
   new_attributes['time_from_work_driving'] = 0
